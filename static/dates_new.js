b9e13b14f16319093ebb795bab88a1dc3307c82b
filser michael <filser.michael@gmail.com>
2023-06-04 08:50:48 +0200

    added weeks

--- static/dates_new.js
+++ static/dates_new.js
@@ -3,8 +3,18 @@ console.log("years", years, "months", months, "days", days)
 var today = new Date();
 var current_day = new Date();
 
+// function weeksBetween(d1, d2) {
+//     return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000));
+// }
+// let weeks = weeksBetween(new Date(2022, 10, 15), new Date());
+
 const birthday = moment.tz("2022-10-15 14:21:00", "Europe/Berlin");
 current_day = moment.tz(current_day, 'DD/MM/YYYY HH:mm:ss', 'Europe/Berlin');
+
+
+weeks=current_day.diff(birthday, 'week');
+console.log("wochen: ", weeks)
+
 const diff = birthday.diff(current_day);
 console.log(birthday, current_day)
 const diffDuration = moment.duration(diff)
@@ -27,14 +37,22 @@ console.log("Hours:", hours)
 console.log("Minutes:", minutes)
 console.log("Seconds:", seconds)
 
+
+
 function insertVars() {
     document.getElementById('years').innerHTML = years
     document.getElementById('months').innerHTML = months
     document.getElementById('days').innerHTML = days
-    document.getElementById('hours').innerHTML = hours
-    document.getElementById('minutes').innerHTML = minutes
-    document.getElementById('seconds').innerHTML = seconds
+    
+    //document.getElementById('hours').innerHTML = hours
+    //document.getElementById('minutes').innerHTML = minutes
+    //document.getElementById('seconds').innerHTML = seconds
+
+    document.getElementById('weeks').innerHTML = weeks
+
 }
+
+
 
 function dateDiff(date) {
     date = date.split('-');
